3e4e4aa9c84b3521a32bd150fc6e6a3cc6c7a1ad
chaeyk <chaeyk@neowiz.com>
2020-02-09 16:39:53 +0900

    log ping

--- src/main/java/net/chaeyk/lbtest/PingController.java
+++ src/main/java/net/chaeyk/lbtest/PingController.java
@@ -1,17 +1,20 @@
 package net.chaeyk.lbtest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ping")
+@Slf4j
 public class PingController {
 
     private int statusCode = HttpStatus.OK.value();
 
     @GetMapping
     public ResponseEntity<String> ping() {
+        log.info("ping");
         return ResponseEntity.status(statusCode).body("OK - " + statusCode);
     }
 
